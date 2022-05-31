3261abcc627c261b3518c94fa318446ceb74f500
siddhantprateek <siddhantprateek@gmail.com>
2022-05-31 21:41:57 +0530

    added filter component

--- client/src/components/Filter/Filter.components.jsx
+++ client/src/components/Filter/Filter.components.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client'
 import './Filter.styles.css';
 
 const Filter = () => {
-  
+  const { loading, error, data }= useQuery(gql`
+   {
+      countries {
+        Country
+        Slug
+      }
+   }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data.countries)
   return (
     <div className='filter-section'>
       <div className="filter-option section-left">
         <select name="" id="">
-          <option value="">Worldwide</option>
+         {
+           data.countries.map((cnt) => (
+             <option value="">{cnt.Country}</option>
+           ))
+         }
         </select>
       </div>
       <div className="pie-chart section-right">

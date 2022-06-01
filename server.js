06874be5f9dde924ede8b18f51182abb0a9fdd74
siddhantprateek <siddhantprateek@gmail.com>
2022-06-01 18:13:52 +0530

    deploy files

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes/routes');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-// const { ApolloServer, gql } = require('apollo-server');
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
-
-// data
-
-
-// The root provides a resolver function for each API endpoint
-
-
 // middleware
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", 'index.html'));
+});
 app.use(express.json())
 app.use(cors())
 
@@ -23,9 +18,9 @@ app.use(cors())
 // routes
 app.use('/', router)
 
-app.get('/', (req, res) => {
-    res.send('server running marvellously')
-})
+// app.get('/api', (req, res) => {
+//     res.send('server running marvellously')
+// })
 
 app.listen(PORT, () =>{
     console.log(`server is running at http://localhost:${PORT}`)

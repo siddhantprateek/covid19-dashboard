aa791a5706316bccf45998ad96e5038fee7ad82c
siddhantprateek <siddhantprateek@gmail.com>
2022-06-01 17:32:25 +0530

    Header component

--- client/src/components/Header/Header.components.jsx
+++ client/src/components/Header/Header.components.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.styles.css';
 
+
 const Header = () => {
+
   return (
-    <div>Header</div>
+    <header className='header-container'>
+      <img src="" alt="logo" />
+      <ul>
+        <li>
+          <Link to="/">
+            Dasboard
+          </Link>
+        </li>
+        <li>
+          <Link to="/safety" >
+            Safety Measures
+          </Link>
+        </li>
+        <li>
+          <Link to="/news">
+            News
+          </Link>
+        </li>
+        <li>
+          <Link to="/aboutus">
+            About Us
+          </Link>
+        </li>
+        <li>
+          <img src="" alt="user" />
+        </li>
+      </ul>
+    </header>
   );
 };
 

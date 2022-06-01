c379a75c7f3eff1dbdf920a1336617af6b55c14e
siddhantprateek <siddhantprateek@gmail.com>
2022-06-01 17:31:02 +0530

    news pages added to client

---
+++ client/src/pages/news/news.pages.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from '../../config/axios';
+import './news.styles.css'
+
+const NewsPage = () => {
+const [newsData, setNewsData] = useState()
+  useEffect(() => {
+    const getNews = async () => {
+      try{
+        const res = await axios.get('/news')
+        setNewsData(res.data.articles)
+      }catch(err){
+        console.error(err)
+      }
+    }
+    getNews()
+  }, [])
+  return (
+    <div className='news-page'>
+        <div className="news-article-section">
+            {
+                newsData?.map((article) => (
+                    <div className="news-box">
+                        <img className="img-thumbnail" src={article.urlToImage} alt="" />
+                        <h4>{article.title}</h4>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
+  )
+}
+
+export default NewsPage
